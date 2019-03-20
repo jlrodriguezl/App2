e629833adc5c3f3d4c74aa3493587cfcbc7cba37
jlrodriguezl <jrodriguez.51c@gmail.com>
2019-03-20 09:55:59 -0500

    Nuevo resgistro de peronas

--- BD/DML_PERSONAS.sql
+++ BD/DML_PERSONAS.sql
@@ -1,3 +1,4 @@
 ﻿-- Crear usuario administrador
 INSERT INTO PERSONAS VALUES
 (12345678, 'JOSE RODRIGUEZ', 3164275405, 'Cra 55A # 187 - 51', 'jrodriguez.51c@gmail.com', null, 'ADM', '1234', 'A');
+--crear usuario empleado
