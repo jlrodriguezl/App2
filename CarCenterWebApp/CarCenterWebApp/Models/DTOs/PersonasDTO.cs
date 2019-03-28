6f123dcce474de66237ce08298262a2a740de440
donal484 <dgm4015@gmail.com>
2019-03-27 20:54:05 -0500

    DTO's y controllers 27-03-19

--- CarCenterWebApp/CarCenterWebApp/Models/DTOs/PersonasDTO.cs
+++ CarCenterWebApp/CarCenterWebApp/Models/DTOs/PersonasDTO.cs
@@ -12,25 +12,45 @@ namespace CarCenterWebApp.Models.DTOs
         [StringLength(15)]
         [Display(Name = "Identificación")]
         public Decimal Identificacion { get; set; }
+
         [Required]
         [StringLength(100)]
         [Display(Name = "Nombres")]
         public String Nombres { get; set; }
+
         [Required]
         [StringLength(10)]
         [Display(Name = "Telefono")]
         public Decimal Telefono { get; set; }
+
         [Required]
-        [StringLength(15)]
-        [Display(Name = "Identificación")]
+        [StringLength(100)]
+        [Display(Name = "Correo")]
         public String Correo { get; set; }
+
         [Required]
-        [StringLength(15)]
-        [Display(Name = "Identificación")]
+        [StringLength(12)]
+        [Display(Name = "Contraseña")]
         public String Contrasena { get; set; }
+
         [Required]
-        [StringLength(15)]
-        [Display(Name = "Identificación")]
-        public String EstadoPersona { get; set; }
+        [StringLength(10)]
+        [Display(Name = "EstadoPersona")]
+        public String Estado_Persona { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Dirección")]
+        public String Direccion { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Especialidad")]
+        public String Especialidad { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Tipo Persona")]
+        public String Tipo_Persona { get; set; }
     }
 }
